7eb25820c53dab177296dddd850614bae9a78bbc
Confidence Okoghenun <confidenceboi@gmail.com>
2020-08-11 07:40:20 +0100

    feat: Fix genre

--- operation.js
+++ operation.js
@@ -32,6 +32,28 @@ const getDupsArtist = async () => {
   ]).then((result) => result);
   log(dupArtists, 'artist');
 };
+const getDupsGenre = async () => {
+  const dupGenres = await Genre.aggregate([
+    {
+      $group: {
+        _id: { name: '$name' },
+        uniqueIds: { $addToSet: '$_id' },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $match: {
+        count: { $gt: 1 },
+      },
+    },
+    {
+      $sort: {
+        count: -1,
+      },
+    },
+  ]).then((result) => result);
+  log(dupGenres, 'genre');
+};
 
 const rmvDupsArtist = async () => {
   const data = fs.readFileSync('./output/artist.json');
@@ -66,6 +88,39 @@ const rmvDupsArtist = async () => {
   });
 };
 
+const rmvDupsGenre = async () => {
+  const data = fs.readFileSync('./output/genre.json');
+  const array = JSON.parse(data);
+
+  await asyncForEach(array, async ({ uniqueIds }, index) => {
+    try {
+      const truId = uniqueIds[0];
+      const dupId = uniqueIds[1];
+
+      const albums = await Album.find({ genre: dupId });
+      console.log(`Found ${albums.length} albums with ID ${dupId}`);
+
+      await asyncForEach(albums, async (album) => {
+        album.genre = album.genre.map((i) =>
+          String(i) === String(dupId) ? truId : i
+        );
+        album.save();
+        console.log(`Removed ID ${dupId} from Album ${album._id}`);
+      });
+
+      await Genre.deleteOne({ _id: dupId });
+      console.log(`Processed Index ${index}`);
+
+      if (index === array.length - 1) {
+        console.log('DONE');
+        process.exit(0);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
 module.exports = async () => {
-  rmvDupsArtist();
+  rmvDupsGenre();
 };
--- utils/log.js
+++ utils/log.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 module.exports = (data, filename) => {
-  fs.writeFile(`./${filename}.json`, JSON.stringify(data), (err) => {
+  fs.writeFile(`../output/${filename}.json`, JSON.stringify(data), (err) => {
     if (err) console.log(err);
     console.log(`Saved ${data.length} records to ${filename}.json`);
     process.exit(0);
